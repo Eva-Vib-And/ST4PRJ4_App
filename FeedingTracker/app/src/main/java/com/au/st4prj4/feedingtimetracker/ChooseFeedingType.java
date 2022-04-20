cb44cda5bbbc7d2346b78c037093f4d8382fcb03
Eva-Vib-And <60734490+Eva-Vib-And@users.noreply.github.com>
2022-04-20 13:38:06 +0200

    RadioButton started

--- FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/ChooseFeedingType.java
+++ FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/ChooseFeedingType.java
@@ -3,6 +3,7 @@ package com.au.st4prj4.feedingtimetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseFeedingType extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class ChooseFeedingType extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_feeding_type);
+
+
+    }
+
+    public void userChoseFeedingType(View view) {
+        
     }
 }
