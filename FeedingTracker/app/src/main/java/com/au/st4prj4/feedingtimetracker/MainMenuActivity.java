4e114c7eade8d91aad0b2c8a4104e9ffe16f7077
SorubaTharumalingam <78963412+SorubaTharumalingam@users.noreply.github.com>
2022-05-17 11:35:05 +0200

    menu oversigt

--- FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/MainMenuActivity.java
+++ FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/MainMenuActivity.java
@@ -2,7 +2,11 @@ package com.au.st4prj4.feedingtimetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button startButton = (Button) (findViewById(R.id.startButton));
+        Button overviewButton = (Button) (findViewById(R.id.overviewButton));
+        Button milkplanButton = (Button) (findViewById(R.id.milkplanButton));
+
+    startButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //intent-objekt indeholder en destination - vi vil gå fra den ene aktivitet til den anden
+            Intent intent = new Intent(MainMenuActivity.this, ChooseFeedingType.class);
+            startActivity(intent);
+        }
+    });
+    overviewButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //intent-objekt indeholder en destination - vi vil gå fra den ene aktivitet til den anden
+            Intent intent = new Intent(MainMenuActivity.this, OverviewAcitivity.class);
+            startActivity(intent);
+        }
+    });
+
+    milkplanButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //intent-objekt indeholder en destination - vi vil gå fra den ene aktivitet til den anden
+            Intent intent = new Intent(MainMenuActivity.this, MilkPlanActivity.class);
+            startActivity(intent);
+        }
+    });
+
     }
 }
