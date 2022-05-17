05ef345271e91044d313d344067c81fd0bf231b5
SorubaTharumalingam <78963412+SorubaTharumalingam@users.noreply.github.com>
2022-05-17 12:52:21 +0200

    start icon

--- FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/StartFeedingByBreast.java
+++ FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/StartFeedingByBreast.java
@@ -1,16 +1,46 @@
 package com.au.st4prj4.feedingtimetracker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
-public class StartFeedingByBreast extends AppCompatActivity {
+import com.au.st4prj4.feedingtimetracker.models.Feeding;
+import com.au.st4prj4.feedingtimetracker.models.FeedingList;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class StartFeedingByBreast extends AppCompatActivity {
+Feeding feeding;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_feeding_data);
 
+        feeding = new Feeding();
         //generate some data
+
+        feeding.setFeedingType("Breast");
+        LocalDate dateTime;
+        dateTime= LocalDate.now();
+        //add data to feeding list to keep track of total feedings
+        FeedingList feedingList = new FeedingList();
+        feedingList.setDate(dateTime);
+           /* if(dateTime != feedingList.getDateTime()) {
+                if(feedingList.getDateTime()==null) {
+                    feedingList.setDate(dateTime);
+                    //make new object
+                }
+            }*/
+        feedingList.getTotaltFeedingsToday().add(feeding);
+
+        Intent intent= new Intent();
+        intent.putExtra("data", 10);
+        finish();
     }
 }
