872c824f8b1655e5cd6919f00eab04dfe7a14abc
SorubaTharumalingam <78963412+SorubaTharumalingam@users.noreply.github.com>
2022-05-17 12:23:01 +0200

    Revert "merged"
    This reverts commit 5a5b8ca35215c456be416837b9a85a2fe7dfd47c, reversing
    changes made to 4e114c7eade8d91aad0b2c8a4104e9ffe16f7077.

--- FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/EditFeedingData.java
+++ FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/EditFeedingData.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StartFeedingByBreast extends AppCompatActivity {
+public class EditFeedingData extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
