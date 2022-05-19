337987b4cfe063556ae3c96110ad8194fe5e9ae8
Eva-Vib-And <60734490+Eva-Vib-And@users.noreply.github.com>
2022-05-19 20:55:23 +0200

    fix en fejl

--- FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/activity/MainMenuActivity.java
+++ FeedingTracker/app/src/main/java/com/au/st4prj4/feedingtimetracker/activity/MainMenuActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
@@ -79,17 +80,19 @@ public class MainMenuActivity extends AppCompatActivity {
             PopupMenu popupMenu = new PopupMenu(this, txt_option);
             popupMenu.inflate(R.menu.option_menu);
 
-
-            popupMenu.setOnMenuItemClickListener(item -> {
-                switch (item.getItemId()) {
-                    case R.id.logout:
-                        mAuth.signOut();
-                        startActivity(new Intent(this, LoginActivity.class));
-                        finish();
-                        break;
-                }
-                return false;
-            });
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                 public boolean onMenuItemClick(MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.logout:
+                            mAuth.signOut();
+                            startActivity(new Intent(MainMenuActivity.this, LoginActivity.class));
+                            finish();
+                            break;
+                    }
+                 return false;
+             }
+        });
             popupMenu.show();
         });
     }
